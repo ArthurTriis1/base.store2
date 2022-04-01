affe066d9989ace0b075f93e575337f064fd37d3
Arthur Andrade <arthurfelandrade@gmail.com>
2022-04-01 12:10:39 -0300

    Initialize test-new-builder

---
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -0,0 +1,67 @@
+import type { SearchEvent } from '@faststore/sdk'
+import {
+  formatSearchState,
+  initSearchState,
+  sendAnalyticsEvent,
+} from '@faststore/sdk'
+import { SearchInput as UISearchInput } from '@faststore/ui'
+import { navigate } from 'gatsby'
+import React from 'react'
+import type {
+  SearchInputProps as UISearchInputProps,
+  SearchInputRef,
+} from '@faststore/ui'
+import { MagnifyingGlass as MagnifyingGlassIcon } from 'phosphor-react'
+import useSearchHistory from 'src/sdk/search/useSeachHistory'
+import './search-input.scss'
+
+declare type SearchInputProps = {
+  onSearchClick?: () => void
+  buttonTestId?: string
+} & Omit<UISearchInputProps, 'onSubmit'>
+
+const doSearch = async (term: string) => {
+  const { pathname, search } = formatSearchState(
+    initSearchState({
+      term,
+      base: '/s',
+    })
+  )
+
+  sendAnalyticsEvent<SearchEvent>({
+    name: 'search',
+    params: { search_term: term },
+  })
+
+  navigate(`${pathname}${search}`)
+}
+
+const SearchInput = React.forwardRef<SearchInputRef, SearchInputProps>(
+  function SearchInput(
+    { onSearchClick, buttonTestId = 'store-search-button', ...props },
+    ref
+  ) {
+    const { addToSearchHistory } = useSearchHistory()
+    const handleSearch = (term: string) => {
+      addToSearchHistory(term)
+      doSearch(term)
+    }
+
+    return (
+      <UISearchInput
+        ref={ref}
+        icon={
+          <MagnifyingGlassIcon
+            onClick={onSearchClick}
+            data-testid={buttonTestId}
+          />
+        }
+        placeholder="Search everything at the store"
+        onSubmit={handleSearch}
+        {...props}
+      />
+    )
+  }
+)
+
+export default SearchInput
